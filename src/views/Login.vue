5600b177f556900e7f56c311614996000e873005
Phantomlsh <phantomlsh@outlook.com>
2021-10-31 09:08:01 -0700

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,8 +21,8 @@ async function next () {
   <div class="h-screen bg-gray-100 flex justify-center items-center">
     <transition name="fade">
       <img v-if="loading" src="/logo.svg" class="absolute w-20 h-20">
-      <div v-else class="absolute w-80 h-60 bg-white shadow-md flex justify-center items-center flex-col rounded transition-all">
-        <h1 class="text-3xl font-bold">学生事务系统</h1>
+      <div v-else class="absolute w-80 h-56 bg-white shadow-md flex justify-center items-center flex-col rounded transition-all">
+        <h1 class="text-2xl font-semibold">学生事务系统</h1>
         <input class="mt-6 mb-4 rounded px-3 py-2 radius-2 border-2 border-gray-300 focus:ring-1 focus:border-blue-300 transition"
           :placeholder="random ? '请输入密码' : '请输入用户名'"
           :type="random ? 'password' : 'text'"
