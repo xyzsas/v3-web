88c7d0da5f1d5390a416cf28f5f226abb0d6ac84
Layzzi <Shadow_sock@icloud.com>
2021-11-06 22:13:11 +0800

    basic look

---
+++ src/plugins/state.js
@@ -0,0 +1,25 @@
+export const U = { name: 's1mple', group: '/NAVI' }
+
+const sentences = {
+  spring: [],
+  summer: [],
+  fall: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, '却道闲愁都几许？一川烟草，满城风絮，梅子黄时雨。', 23],
+  winter: []
+}
+
+
+export const greet = (() => {
+  const date = new Date()
+  const ret = {}
+  const h = date.getHours()
+  if (h < 11) ret.g = '早上好'
+  if (h >= 11 && h < 13) ret.g = '中午好'
+  if (h >= 13 && h < 18) ret.g = '下午好'
+  if (h >= 18) ret.g = '晚上好'
+  const m = date.getMonth()
+  if (m < 3 || m >= 12) ret.s = sentences.winter[h]
+  if (m >= 3 && m < 6) ret.s = sentences.spring[h]
+  if (m >= 6 && m < 9) ret.s = sentences.summmer[h]
+  if (m >= 9 && m < 12) ret.s = sentences.fall[h]
+  return ret
+})()
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+import { U, greet } from '../plugins/state.js'
+const router = useRouter(), route = useRoute()
+// if (!U.value) router.push('/login')
+</script>
+
+<template>
+  <div class="h-screen bg-gray-100">
+    <div class="pr-24 pl-24 pt-12" v-if="U">
+      <h1 class="text-3xl font-mono font-medium mt-3">{{ greet.g }}, {{ U.name }}!</h1>
+      <p class="text-1xl font-mono mt-3">用户组：<code class="text-red-400 p-1 bg-gray-200 rounded-md">{{ U.group }}</code></p>
+      <p class="text-1xl font-serif text-gray-500 mt-3">--{{ greet.s }}</p>
+    </div>
+  </div>
+</template>
