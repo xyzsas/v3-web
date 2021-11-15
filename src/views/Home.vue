c383d6e1c8cb9a2fb9b5484dc1d4f0b12f946c79
CutePikachu <z5191511@ad.unsw.edu.au>
2021-11-15 23:55:00 +1100

    fix home page name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@ onMounted(() => {
 <template>
   <div class="h-screen bg-gray-100">
     <div class="px-10 py-8">
-      <h1 class="text-3xl font-medium mt-3">{{ greet.g }}, s1mple!</h1>
+      <h1 class="text-3xl font-medium mt-3">{{ greet.g }}, {{ user.name }}!</h1>
       <p :class="trans" class="mt-1 text-1xl font-serif text-gray-500" style="transition: all 2s ease;">
         {{ greet.s }}
       </p>
