a307a55a21f8802a717f44b263f50c51555b0aae
Phantomlsh <phantomlsh@outlook.com>
2021-11-14 21:17:37 -0800

    Update Wrapper.vue

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -6,7 +6,7 @@ let innerHeight = $computed(() => inner && window.getComputedStyle(inner).getPro
 </script>
 
 <template>
-  <div ref="outer" style="overflow-y: hidden;" :style="{ transition: props.transition || 'all 0.5s ease', height: props.show ? innerHeight : '0px' }">
+  <div ref="outer" style="overflow-y: hidden;" :style="{ transition: props.transition || 'all 0.5s ease', height: props.show ? innerHeight : '0px', opacity: props.show ? 1 : 0 }">
     <div ref="inner" :class="props.class">
       <slot></slot>
     </div>
