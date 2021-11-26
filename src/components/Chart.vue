f95c5a62bfe4887f7715c906f48fde3579b3a107
Phantomlsh <phantomlsh@outlook.com>
2021-11-25 19:18:50 -0800

    Update chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,65 +1,38 @@
-<template>
-  <div>
-    <svg viewBox="-10 -10 420 320">
-      <path d="M0,0L100,100" />
-      <g v-for="(d, i) in data">
-        <path :d="lines[i]" stroke="teal"/>
-        <circle :r="rs[i]" :cx="i * interval" fill="#1f78b4">
-          <animate attributeName="cy" from="300" :to="300 - data[i]" dur="0.3s" repeatCount="1" fill="freeze" />
-        </circle>
-      </g>
-      <path class="cross" :d="path" stroke="teal" fill="none" />
-    </svg>
-  </div>
-</template>
 <script setup>
+import { watch } from 'vue'
 let data = $ref([92, 101, 130, 111, 121, 151, 97, 112, 160, 120, 83, 96, 146, 132, 150, 150, 144])
-let rs = $ref([])
-let lines = $ref([])
-const interval = 400 / (data.length - 1)
-const sqrt = d3.scaleSqrt().domain([80, 160]).range([2, 4])
+const w = 10, h = 50
+let viewBox = $ref('')
+let parsed = $ref([])
 
-for (const i in data) {
-  rs.push(sqrt(data[i]))
-  lines.push(d3.line()([[i * interval, 0], [i * interval, 300]]))
-}
-let path = $ref('')
-let i = 0
-path = d3.line()
-  .x(d => i++ * interval)
-  .y(d => (300 - d))(data)
-
-console.log(path)
-
-</script>
-<style scoped>
-svg {
-  width: 800px;
-  background-color: rgba(243, 244, 246);
-}
-path {
-  stroke-dasharray: 2;
-}
-.cross {
-  stroke-dasharray: 600;
-  stroke-dashoffset: 600;
-  animation: offset 0.3s ease;
-  animation-delay: 0.3s;
-  animation-fill-mode: forwards;
-}
-@keyframes offset {
-  to {
-    stroke-dashoffset: 0;
+const sleep = ms => new Promise(r => setTimeout(r, ms))
+watch($$(data), async () => {
+  parsed.splice(0, parsed.length)
+  const min = Math.min(...data), max = Math.max(...data), L = max - min
+  for (const d of data) parsed.push([h + 2*w, 0])
+  viewBox = `-${w} -${w} ${data.length*w + 2*w} ${h + 2*w}`
+  for (let i = 0; i < data.length; i++) {
+    await sleep(80)
+    const n = (max-data[i]) / L
+    parsed[i] = [h * n, Math.sqrt(1 - n)/2 + 0.5]
   }
-}
+}, { immediate: true })
+
+const style = (p, i) => `transform: translate(${i * w}px, ${p[0]}px) scale(${p[1]});`
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" role="img" :viewBox="viewBox" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <line v-for="(p, i) in parsed" :x1="i*w" :y1="h+2*w" :x2="i*w" :y2="p[0]" stroke="#ff0000" stroke-dasharray="2 1" :style="{ opacity: p[1]/2 }" :key="i"></line>
+    <circle v-for="(p, i) in parsed" r="2" fill="#ff0000" :style="style(p, i)" :key="i"></circle>
+  </svg>
+</template>
+
+<style scoped>
 circle {
-  stroke: rgba(117, 183, 130);
-  stroke-width: 1px;
-  stroke-linejoin: round;
-  animation: hover 0.1s linear;
-  animation-fill-mode: forwards;
+  transition: all 0.5s ease;
 }
-circle:hover {
-  stroke-width: 3px;
+line {
+  transition: all 0.5s ease 0.5s;
 }
 </style>
