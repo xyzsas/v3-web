8f51fd4e3524f8e50932af65345659151051ad43
Phantomlsh <phantomlsh@outlook.com>
2021-11-27 11:20:52 -0800

    Update wrapper
    Allow nested wrapper

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,27 @@
 <script setup>
+import Wrapper from '../components/Wrapper.vue'
 const LS = window.localStorage
 const user = (LS['web-user-version'] || 'Unavailable').replace(/-/g, '--')
 const admin = (LS['web-admin-version'] || 'Unavailable').replace(/-/g, '--')
+
+let show = $ref(false)
+setTimeout(() => { show = true }, 500)
 </script>
 
 <template>
   <div class="w-5/6 m-auto min-h-screen flex flex-col justify-center items-center">
-    <div class="w-screen max-w-2xl relative group flex justify-center items-center">
+    <div class="w-screen max-w-2xl relative group flex justify-center items-center mb-6">
       <img style="transition: all 0.5s ease;" src="/X.svg" class="absolute w-1/3 group-hover:left-0 left-1/3">
       <img src="/Y.svg" class="w-1/3">
       <img style="transition: all 0.5s ease;" src="/Z.svg" class="absolute w-1/3 group-hover:left-2/3 left-1/3">
     </div>
-    <h1 class="text-5xl mt-6">XYZSAS</h1>
-    <p class="cursor-pointer">Powered by yzITI</p>
-    <div class="flex flex-col items-center justify-around h-20 m-3">
-      <img :src="`https://img.shields.io/badge/user-${user}-blue`">
-      <img :src="`https://img.shields.io/badge/admin-${admin}-red`">
-    </div>
+    <wrapper :show="show">
+      <h1 class="text-5xl">XYZSAS</h1>
+      <p class="cursor-pointer text-center mb-3">Powered by yzITI</p>
+      <div class="flex flex-col items-center justify-around h-20">
+        <img :src="`https://img.shields.io/badge/user-${user}-blue`">
+        <img :src="`https://img.shields.io/badge/admin-${admin}-red`">
+      </div>
+    </wrapper>
   </div>
-  
 </template>
