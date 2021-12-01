71262f54fccb242871766815f5768873362abbe6
CutePikachu <z5191511@ad.unsw.edu.au>
2021-12-01 14:13:17 +1100

    add new block

---
+++ src/blocks/Markdown.vue
@@ -0,0 +1,9 @@
+<script setup>
+import snarkdown from 'snarkdown'
+const { value } = defineProps(['value', 'index', 'state'])
+let html = $computed(() => snarkdown(value))
+</script>
+
+<template>
+  <div v-html="html" />
+</template>
---
+++ src/views/Affair.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineAsyncComponent } from 'vue'
+
+const blocks = {
+  markdown: defineAsyncComponent(() => import('../blocks/Markdown.vue'))
+}
+const content = [{
+  _: 'markdown',
+  ':': '_this_ is **easy** to `use`.'
+}]
+</script>
+
+<template>
+  <div v-for="(b, i) in content" :key="i">
+    <component :is="blocks[b._]" :value="b[':']" :index="i"></component>
+  </div>
+</template>
