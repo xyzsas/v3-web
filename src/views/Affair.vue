4057435d2e283e12cf4e597022519260507ea9ed
Layzzi <Shadow_sock@icloud.com>
2021-12-02 23:26:52 +0800

    maybe like this?
    idk
    i think it might look good

--- src/views/Affair.vue
+++ src/views/Affair.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineAsyncComponent } from 'vue'
+import { ArrowLeftIcon } from '@heroicons/vue/solid'
 
 const blocks = {
   markdown: defineAsyncComponent(() => import('../blocks/Markdown.vue'))
@@ -8,10 +9,25 @@ const content = [{
   _: 'markdown',
   ':': '_this_ is **easy** to `use`.'
 }]
+
+for (let i = 1; i <= 40; i++) content.push({
+  _: 'markdown',
+  ':': '_this_ is **easy** to `use`.'
+})
+
 </script>
 
 <template>
-  <div v-for="(b, i) in content" :key="i">
-    <component :is="blocks[b._]" :value="b[':']" :index="i"></component>
+  <div class="h-screen w-screen bg-gray-100 px-10 py-8">
+    <h1 class="text-3xl font-medium m-3 flex items-center mb-6">
+      <arrow-left-icon style="transition: all 0.5s ease;" class="w-8 ml-2 mr-3 hover:ml-0 hover:mr-5 cursor-pointer" @click="router.push('/')"></arrow-left-icon>
+      事务
+    </h1>
+    <div class=" w-full h-4/5 overflow-y-auto py-4 bg-white shadow-md rounded m-1 p-8 flex flex-col items-center md:items-start">
+      <div v-for="(b, i) in content" :key="i">
+        <component :is="blocks[b._]" :value="b[':']" :index="i"></component>
+      </div>
+    </div>
   </div>
 </template>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
 import { useRouter } from 'vue-router'
 import { user } from '../state.js'
 import { greet } from '../utils/greet.js'
+import { FingerPrintIcon } from '@heroicons/vue/outline'
 const router = useRouter()
 if (!user.token) router.push('/login')
 
@@ -12,10 +13,16 @@ setTimeout(() => { trans = 'opacity-100' }, 1000)
 <template>
   <div class="h-screen bg-gray-100" v-if="user.token">
     <div class="px-10 py-8">
-      <h1 class="text-3xl font-medium mt-3">{{ greet.g }}, {{ user.name }}!</h1>
-      <p :class="trans" class="mt-1 text-1xl font-serif text-gray-500" style="transition: all 2s ease;">
-        {{ greet.s }}
-      </p>
+      <div class="mb-3">
+        <h1 class="text-3xl font-medium mt-3">{{ greet.g }}, {{ user.name }}!</h1>
+        <p :class="trans" class="mt-1 text-1xl font-serif text-gray-500" style="transition: all 2s ease;">
+          {{ greet.s }}
+        </p>
+      </div>
+      <button class="transition flex items-center rounded py-2 px-4 shadow-md bg-white font-bold hover:bg-red-200" @click="router.push('/security')">
+        <finger-print-icon class="w-6 h-6 text-red-500"/>
+        安全中心
+      </button>
     </div>
   </div>
 </template>
