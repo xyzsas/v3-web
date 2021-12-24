d5e1d8b0409c8f21ec17f3166795ff81cd3487fd
Phantomlsh <phantomlsh@outlook.com>
2021-12-24 07:10:30 -0800

    Update admin/affair UI

--- src/views/admin/Affair.vue
+++ src/views/admin/Affair.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { ArrowDownIcon, ArrowUpIcon, PlusIcon, TrashIcon } from '@heroicons/vue/outline'
 import Wrapper from '../../components/Wrapper.vue'
@@ -76,7 +76,7 @@ async function remove () {
 
 // following are for editor
 let focus = $ref(0)
-const panelShow = reactive([1, 0, 1])
+const panelShow = reactive([1, 0, 0])
 let focused = $computed(() => affair.content[focus] || {})
 
 function swap (i, j) {
@@ -106,31 +106,31 @@ function del (i) {
           <button class="all-transition bg-red-700 px-3 py-1 m-1 text-white rounded hover:shadow-md" @click="remove">删除事务</button>
         </p>
       </panel-wrapper>
-      <panel-wrapper title="组件属性" v-model="panelShow[1]">
-        <transition name="fade" mode="out-in">
-          <component style="opacity: 0.6;" v-if="blocks[focused._].panel" :is="blocks[focused._].panel" :i="focus"></component>
-          <p class="p-3 text-gray-400" v-else>没有需要配置的属性</p>
-        </transition>
-      </panel-wrapper>
-      <panel-wrapper title="添加组件" v-model="panelShow[2]">
-        <div class="flex flex-wrap opacity-60">
-          <div class="flex flex-col items-center w-20 m-2 cursor-pointer" v-for="(v, k) in blocks" @click="add(k)">
-            <img class="w-10" :src="v.icon">
-            <p>{{ v.name }}</p>
+      <panel-wrapper title="添加组件" v-model="panelShow[1]">
+        <div class="flex flex-wrap opacity-60 p-3">
+          <div class="flex flex-col items-center w-16 cursor-pointer" v-for="(v, k) in blocks" @click="add(k)">
+            <img class="w-9" :src="v.icon">
+            <p class="text-sm">{{ v.name }}</p>
           </div>
         </div>
+      </panel-wrapper>
+      <panel-wrapper title="组件属性" v-model="panelShow[2]">
+        <div v-if="blocks[focused._].panel" style="opacity: 0.6;">
+          <component :is="blocks[focused._].panel" :i="focus" />
+        </div>
+        <p class="p-3 text-gray-400" v-else>没有需要配置的属性</p>
       </panel-wrapper>
     </div>
     <!-- Preview -->
     <div class="bg-gray-100 h-auto md:w-2/3 min-h-screen md:h-screen p-4 lg:px-20 lg:py-8 overflow-y-auto">
       <input class="text-2xl m-3 mb-6 bg-transparent w-full" v-model="affair.title">
-      <div v-for="(b, i) in affair.content" :key="b" class="m-1 bg-white rounded all-transition" :class="{ 'shadow': focus == i }" @click="focus = i; panelShow[1] = 1">
+      <div v-for="(b, i) in affair.content" :key="b" class="m-1 bg-white rounded all-transition" :class="{ 'shadow': focus == i }" @click="focus = i; panelShow[2] = 1">
         <component :is="blocks[b._].editable || blocks[b._].block" :i="i"></component>
         <wrapper :show="focus == i" class="flex justify-end">
           <arrow-up-icon v-if="i > 0" class="w-6 m-2 cursor-pointer text-gray-500" @click="swap(i, i-1)" />
           <arrow-down-icon v-if="i+1 < affair.content.length" class="w-6 m-2 cursor-pointer text-gray-500" @click="swap(i, i+1)"/>
           <trash-icon v-if="affair.content.length > 1" class="w-6 m-2 cursor-pointer text-red-500" @click="del(i)"/>
-          <plus-icon class="w-6 m-2 cursor-pointer text-blue-500" @click="panelShow[0] = 0; panelShow[2] = 1" />
+          <plus-icon class="w-6 m-2 cursor-pointer text-blue-500" @click="panelShow[0] = 0; panelShow[1] = 1" />
         </wrapper>
       </div>
     </div>
--- src/views/admin/XYZ.vue
+++ src/views/admin/XYZ.vue
@@ -23,7 +23,7 @@ else fetch()
   <div class="p-5 sm:p-10 min-h-screen bg-gray-100">
     <overlay-loading :show="loading"></overlay-loading>
     <h1 class="text-2xl font-bold flex items-center mb-6"><arrow-left-icon class="all-transition w-12 pl-2 pr-3 hover:pl-0 hover:pr-5 cursor-pointer" @click="router.push('/')"></arrow-left-icon>事务管理</h1>
-    <p v-if="!list">暂无事务</p>
+    <p v-if="!list">暂无可以管理的事务</p>
     <div v-for="(v, k) in list" :key="k" class="bg-white rounded py-4 px-6 shadow cursor-pointer flex items-center m-3" @click="router.push('/admin/affair/' + k)">
       <h3 class="text-xl flex items-center">{{ v }}</h3>
     </div>
