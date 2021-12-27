56b7307a2be18479a078c2cbfa7f0aec4f089572
Phantomlsh <phantomlsh@outlook.com>
2021-12-27 08:44:20 -0800

    Affair submit

--- src/views/Affair.vue
+++ src/views/Affair.vue
@@ -3,15 +3,16 @@ import { reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import Wrapper from '../components/Wrapper.vue'
 import OverlayLoading from '../components/OverlayLoading.vue'
+import { CheckIcon } from '@heroicons/vue/solid'
 import blocks from '../blocks/index.js'
 import { affair, user } from '../state.js'
 import request from '../utils/request.js'
 
 const router = useRouter(), route = useRoute()
 let loading = $ref(true)
+const opt = user.token ? { headers: { token: user.token } } : {}
 
 async function fetch () {
-  const opt = user.token ? { headers: { token: user.token } } : {}
   const res = await request.get('/xyz/affair/' + route.params.id, opt)
   if (!res) {
     if (window.lastUEC == 'XYZSAS-0021') router.go(-1)
@@ -22,10 +23,17 @@ async function fetch () {
   affair.response = {}
   affair.title = res.title
   affair.content = JSON.parse(res.content)
-  affair.data = res.data || {}
+  affair.data = JSON.parse(res.data || '{}')
   loading = false
 }
 fetch()
+
+async function submit () {
+  loading = true
+  const res = await request.post('/xyz/affair/' + route.params.id, affair.data, opt)
+  if (res) await Swal.fire('提交成功', '', 'success')
+  loading = false
+}
 </script>
 
 <template>
@@ -37,6 +45,7 @@ fetch()
         <component :is="blocks[b._].block" :i="i"></component>
       </wrapper>
     </div>
+    <button @click="submit" class="text-white font-bold w-full sm:w-32 py-2 my-8 sm:mx-4 rounded shadow flex items-center justify-center all-transition hover:shadow-md bg-blue-500"><check-icon class="w-6 mr-2" />提交<div class="w-4" /></button>
   </div>
 </template>
 
