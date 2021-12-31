8efc01a1962f905941b9d55a0422c502930e6520
CutePikachu <z5191511@ad.unsw.edu.au>
2021-12-31 11:22:29 +1100

    add userinfo

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -2,10 +2,14 @@
 import { watch } from 'vue'
 import { user } from '../state.js'
 import request from '../utils/request.js'
+import { short, sha256 } from '../utils/crypto.js'
 import { CheckIcon } from '@heroicons/vue/outline'
+import OverlayLoading from './OverlayLoading.vue'
+
 const props = defineProps(['user'])
-let edit = $ref(null)
-if (props.user == 'NEW') edit = { group: user.group, aauth: {}, affair: {} }
+let edit = $ref({}), loading = $ref(false), userid = $ref(props.user)
+
+if (userid == 'NEW') edit = { group: user.group, aauth: {}, affair: {} }
 else fetch()
 
 watch(() => edit.group, (n, o) => {
@@ -13,7 +17,7 @@ watch(() => edit.group, (n, o) => {
 })
 
 let ready = $computed(() => {
-  if (props.user === 'NEW') {
+  if (userid === 'NEW') {
     if (!edit.username) return false
   }
   if (!edit.name) return false
@@ -22,7 +26,7 @@ let ready = $computed(() => {
 })
 
 async function fetch () {
-  const res = await request.get('/sas/admin/' + props.user, { headers: { token: user.token } })
+  const res = await request.get('/sas/admin/' + userid, { headers: { token: user.token } })
   if (res) {
     res.aauth = JSON.parse(res.aauth || '{}')
     res.affair = JSON.parse(res.affair || '{}')
@@ -31,19 +35,36 @@ async function fetch () {
 }
 
 async function submit () {
+  if (!ready) return
+  loading = true
+  const id = userid === 'NEW' ? short(await sha256(edit.username.toUpperCase())) + '.0' : userid
+  const body = {
+    name: edit.name,
+    password: edit.password,
+    group: edit.group,
+    aauth: JSON.stringify(edit.aauth),
+    affair: JSON.stringify(edit.affair),
+    admin: JSON.stringify(edit.admin)
+  }
+  const res = await request.post('/sas/admin/' + id, body, { headers: { token: user.token } })
+  if (res) {
+    Swal.fire('提交成功', '', 'success')
+    userid = res
+  }
+  loading = false
 }
 </script>
 
 <template>
   <h2 class="text-2xl">用户信息</h2>
-  <code class="text-gray-500">{{ props.user == 'NEW' ? '新用户' : props.user }}</code>
-  <p v-if="!edit">正在载入...</p>
-  <div v-else class="flex flex-col p-3">
-    <label v-if="props.user == 'NEW'">用户名：<input placeholder="登录用户名" v-model="edit.username"></label>
+  <code class="text-gray-500">{{ userid == 'NEW' ? '新用户' : userid }}</code>
+  <OverlayLoading :show="!edit.group || loading" absolute="true" class="opacity-80"/>
+  <div v-if="edit.group" class="flex flex-col p-3">
+    <label v-if="userid == 'NEW'">用户名：<input placeholder="登录用户名" v-model="edit.username"></label>
     <label>姓名：<input placeholder="用户姓名" v-model="edit.name"></label>
     <label>用户组：<input placeholder="/group/id/" v-model="edit.group"></label>
-    <label v-if="props.user != 'NEW'">已激活<input class="m-2" type="checkbox" v-model="edit.password"></label>
-    <button @click="submit" class="text-white font-bold w-32 py-2 my-8 mx-4 rounded shadow flex items-center justify-center all-transition hover:shadow-md" :class="ready ? 'bg-blue-400' : 'bg-gray-400'"><check-icon class="w-6 mr-2" />提交<div class="w-4" /></button>
+    <label v-if="userid != 'NEW'">已激活<input class="m-2" type="checkbox" v-model="edit.password"></label>
+    <button @click="submit" class="text-white font-bold w-32 py-2 my-8 mx-4 rounded shadow flex items-center justify-center all-transition hover:shadow-md" :class="ready ? 'bg-blue-400' : 'bg-gray-400'" :disabled="!ready"><check-icon class="w-6 mr-2" />提交<div class="w-4" /></button>
   </div>  
 </template>
 
--- src/views/admin/UserGroup.vue
+++ src/views/admin/UserGroup.vue
@@ -2,14 +2,23 @@
 import { useRoute, useRouter } from 'vue-router'
 import OverlayLoading from '../../components/OverlayLoading.vue'
 import UserInfo from '../../components/UserInfo.vue'
-import { user } from '../../state.js'
+import { user, group } from '../../state.js'
 import { MenuIcon, ArrowLeftIcon, PlusIcon } from '@heroicons/vue/outline'
+import request from '../../utils/request'
 
 const route = useRoute(), router = useRouter()
 let loading = $ref(false)
 
 if (!user.token || !user.admin?.affair) router.push('/')
+else fetch()
 
+async function fetch () {
+  loading = true
+  const res = await request.get('/sas/admin', { headers: { token: user.token } })
+  group.data = res
+  console.log(res)
+  loading = false
+}
 // UI
 let isMobile = $ref(false), focus = $ref('')
 window.onresize = () => { isMobile = window.innerWidth < 768 }
@@ -24,19 +33,19 @@ window.onresize()
       <div v-if="isMobile && focus" @click="focus = ''" class="absolute w-full h-screen bg-black opacity-30 z-10" />
     </transition>
     <!-- Main View -->
-    <div class="all-transition p-4 w-full h-screen overflow-y-auto lg:py-8 overflow-y-auto relative" :class="focus ? 'md:pr-100' : ''">
+    <div class="all-transition p-4 w-full h-screen overflow-y-auto lg:py-8 relative" :class="!isMobile && focus ? 'md:pr-100' : ''">
       <h1 class="text-2xl font-bold flex items-center mb-6"><arrow-left-icon class="all-transition w-12 pl-2 pr-3 hover:pl-0 hover:pr-5 cursor-pointer" @click="router.push('/')" />用户管理</h1>
       <div>
         <input placeholder="Search Box">
       </div>
       <div>/breadcrumb/breadcrumb/breadcrumb/</div>
-      <div v-for="i in 5" class="gradient-card p-2 m-2">
-        一些列表{{ i }}
+      <div class="gradient-card p-2 m-2" @click="focus='dhDSHoW5F3.0'">
+        /
       </div>
     </div>
     <!-- User -->
     <div class="all-transition bg-white w-96 h-screen overflow-y-auto absolute top-0 z-20 p-4 md:p-6" style="max-width: 90%;" :style="{ right: focus ? 0 : '-25rem' }">
-      <user-info v-if="focus" :user="focus"/>
+      <user-info v-if="focus" :user="focus" :key="focus"/>
     </div>
     <button class="all-transition fixed z-30 right-20 bottom-20 rounded-full bg-blue-500 p-3 md:p-4 shadow-md hover:shadow-xl group">
       <plus-icon class="all-transition w-10 text-white group-hover:rotate-180" @click="focus = 'NEW'"/>
