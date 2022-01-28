c3334851e2366a67427e89eb289e0e136d082c6a
pikapikapikachu6 <939910620@qq.com>
2022-01-28 20:38:34 +0800

    change the block list into click style

--- src/blocks/List.vue
+++ src/blocks/List.vue
@@ -4,25 +4,28 @@ const affair = state.affair
 const { i } = defineProps(['i'])
 const key = affair.content[i]['#']
 const value = affair.content[i][':']
-const result = []
-function check () {
-  for (const k in value.options) {
-    const current = value.options[k]
-    if (current.optional && result.indexOf(value.options[k]['#']) == -1) result.push(value.options[k]['#'])
-    if (!current.optional && result.indexOf(value.options[k]['#']) != -1) result.splice(result.indexOf(value.options[k]['#']), 1)
-    affair.ok[key] = result
+affair.data[key] = []
+function change (cur) {
+  cur.current = !cur.current
+  if (!cur.current && affair.data[key].indexOf(cur['#']) != -1) {
+    affair.data[key].splice(affair.data[key].indexOf(cur['#']), 1)
   }
+  if (cur.current && affair.data[key].indexOf(cur['#']) == -1) {
+    affair.data[key].push(cur['#'])
+  }
+  console.log(affair.data[key]);
 }
 </script>
 
 <template>
   <div class="px-3 py-2">
     <label class="font-semibold mr-3 my-1">{{ value.title }}</label>
-    <div class="flex flex-col mr-3 my-1" v-for="o in value.options">
-        <label class="flex flex-row">
-          <input type="checkbox" value="o[':']" class="ml-2 my-1 mr-1" v-model="o.optional" @change="check()"> 
+    <div class="my-3 w-full bg-white rounded-lg shadow-lg lg:w-1/3">
+      <ul class="divide-y-2 divide-gray-100" v-for="o in value.options">
+        <li @click="change(o)" class="p-3 all-transition bg-gradient-to-r to-white border border-gray-200" :class="o.current ? 'from-blue-300' : 'from-white'"> 
           {{ o[':'] }}
-        </label>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
