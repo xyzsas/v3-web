4adbf136180e483c338dfa322e5727910a2f62b5
pikapikapikachu6 <939910620@qq.com>
2022-01-30 00:04:11 +0800

    merge the HTML branch

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -7,6 +7,7 @@ const blocks = {
     name: '段落文本',
     icon: '/img/HTML.svg',
     block: () => import('./HTML.vue'),
+    panel: () => import('./HTMLPanel.vue'),
     editable: () => import('./HTMLEditable.vue'),
     template: { html: '<h1>欢迎使用学生事务系统</h1><p>这是一个段落文本组件，可以用于显示文字等。</p><p>您可以直接修改该组件上的文字，内容会在<b>光标离开该组件时</b>更新和保存。</p><p>该组件支持HTML和Markdown语法。</p>' }
   },
