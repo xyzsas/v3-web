26ae47d053fb6bc8a600ab8cd7ea7ae05a3579f4
Phantomlsh <phantomlsh@outlook.com>
2022-02-04 18:43:04 -0800

    Fix check Dep

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,9 +9,16 @@ const router = useRouter(), route = useRoute()
 
 import state from '../state.js'
 const user = state.user
-state.loading = true
-if (!window.crypto || !window.crypto.subtle) Swal.fire('浏览器不兼容', '您的浏览器缺少某些功能，会导致您的账户存在安全风险。推荐使用最新版的Google Chrome浏览器。', 'error')
-else state.loading = false
+state.loading = false
+async function checkDep () {
+  try {
+    short(await sha256('test'))
+  } catch (e) {
+    console.log(e)
+    Swal.fire('浏览器不兼容', '您的浏览器缺少某些功能，会导致您的账户存在安全风险。推荐使用最新版的Google Chrome浏览器。', 'error')
+  } 
+}
+checkDep()
 
 let input = $ref('')
 let random = $ref('')
