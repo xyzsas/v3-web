ac9d163353cf82836fdcfca2de6fef9ee309c205
Phantomlsh <phantomlsh@outlook.com>
2022-02-22 10:23:20 -0800

    Add electron
    remove photo

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # xyzsas/v3-web
 
 Please see document <https://github.com/xyzsas/doc>
+
+## Electron
+
+```
+npm run build
+npm run electron
+npm run pack
+```
--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ const index = {
   '/': () => import('./views/Home.vue'),
   '/login': () => import('./views/Login.vue'),
   '/security': () => import('./views/Security.vue'),
-  '/photo': () => import('./views/Photo.vue'),
   '/@/:id': () => import('./views/Affair.vue'),
   '/about': () => import('./views/About.vue'),
   '/grade': () => import('./views/Grade.vue'),
