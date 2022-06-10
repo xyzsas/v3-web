03b0922ccd08c256a4c7eb6c32b8fc99d0b49dd6
CutePikachu <z5191511@ad.unsw.edu.au>
2022-06-10 21:18:22 +1000

    v3-next init

--- src/blocks/Error.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="p-3 text-red-400">组件载入错误！</div>
-</template>
--- src/router.js
+++ src/router.js
@@ -2,16 +2,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const index = {
   '/': () => import('./views/Home.vue'),
-  '/login': () => import('./views/Login.vue'),
-  '/security': () => import('./views/Security.vue'),
-  '/@/:id': () => import('./views/Affair.vue'),
   '/about': () => import('./views/About.vue'),
-  '/grade': () => import('./views/Grade.vue'),
   // admin page
-  '/admin/sas': () => import('./views/admin/SAS.vue'),
-  '/admin/xyz': () => import('./views/admin/XYZ.vue'),
-  '/admin/@/:id': () => import('./views/admin/Affair.vue'),
-  '/admin/data/:id': () => import('./views/admin/Data.vue')
 }
 
 const routes = []
