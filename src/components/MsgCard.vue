ab497583a1b8df472d406b6e0d8e19088f98aac4
Phantomlsh <phantomlsh@outlook.com>
2022-07-18 09:52:02 -0700

    Update MsgCard.vue

--- src/components/MsgCard.vue
+++ src/components/MsgCard.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/solid'
 import state from '../state.js'
 import srpc from '../utils/srpc-fc.js'
 const { _id } = defineProps(['_id'])
@@ -8,19 +7,19 @@ const parseDate = t => moment(t).fromNow()
 const short = (s, l = 32) => s.length > l ? (s.substring(0, l - 2) + '...') : s
 const content = JSON.parse(msg.content)
 
-async function read() {
-  if (await srpc.X.read(state.user.token, msg.time)) state.msgs[_id].read = 1
+async function read () {
+  if (await srpc.X.set(state.user.token, msg.time, '已读')) state.msgs[_id].status = '已读'
 }
 </script>
 
 <template>
-  <div class="w-full bg-white rounded-lg m-2 cursor-pointer p-2 all-transition shadow hover:shadow-md flex justify-between items-center" :class="msg.read || 'ring font-bold bg-blue-50'" @click="read">
+  <div class="w-full bg-white rounded-lg m-2 cursor-pointer p-2 all-transition shadow hover:shadow-md flex justify-between items-center" :class="msg.status || 'ring font-bold bg-blue-50'" @click="read">
     <div>
       <h1 class="mx-2">{{ short(content.title) }}
         <span class="px-1 ml-2 bg-blue-100 rounded text-xs" v-for="t in content.tags" :key="t">{{ t }}</span>
       </h1>
       <p class="mt-2 px-2 text-xs text-gray-500">{{ parseDate(msg.time) }}</p>
     </div>
-    <check-circle-icon class="w-7 text-blue-500" v-if="msg.read" />
+    <div v-if="msg.status" class="text-white font-bold rounded-full text-xs px-2 py-1" :class="msg.status === '已读' ? 'bg-blue-500' : 'bg-green-600'">{{ msg.status }}</div>
   </div>
 </template>
