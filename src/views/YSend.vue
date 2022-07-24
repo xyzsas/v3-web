e5bcaf5c2d779e6c3fe3175830cb467a1344e52a
Tina Ji <z5191511@ad.unsw.edu.au>
2022-07-24 10:48:12 +1000

    update ysend

--- src/views/YSend.vue
+++ src/views/YSend.vue
@@ -5,54 +5,53 @@ import state from '../state.js'
 import BackHeader from '../components/BackHeader.vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
-
+let uids = $ref('')
 let data = $ref({
-  uid: ['用户ID', 'id1, id2, id3', ''],
   title: ['标题', '标题', ''],
-  link: ['外链', '系统外部链接', ''],
-  router: ['内链', '系统内部链接', '']
+  router: ['内链（可选）', '系统内部链接', ''],
+  link: ['外链（可选）', '系统外部链接', ''],
+  tags: ['标签（可选）', 'tag1,tag2', '']
 })
 let msg = $ref({})
 const user = state.user
 
+let ready = $computed(() => uids.match(/\S/) && data.title[2]?.match(/\S/))
+
 async function sendMsg() {
-  if (!data.uid[2]?.length || !data.title[2]?.length || (!data.link[2]?.length && !data.router[2]?.length)) {
-    Swal.fire('请填写消息信息', '', 'error')
-    return
-  }
-  // assmue the links will be in a valid format
-  const keys = Object.keys(data).filter(d => d !== 'uid')
-  const vals = Object.values(data).filter(d => d[0] !== '用户ID').map(d => d[2])
+  state.loading = true
   const content = {}
-  for (const d of keys) content[d] = vals.shift()
-  if (await srpc.Y.send(user.token, [data.uid[2]], JSON.stringify(content), Date.now())) Swal.fire('消息发送成功', '', 'success')
+  for (const d in data) {
+    content[d] = data[d][2]
+  }
+  content.tags = content.tags.split(',').filter(d => d.match(/\S/))
+  const u = uids.replace(/\s/g, '').split(',').filter(d => d.match(/\S/))
+  const res = await srpc.Y.send(user.token, u, JSON.stringify(content), Date.now())
+  if (res) Swal.fire('消息发送成功', '', 'success')
   else Swal.fire('消息发送失败', '', 'error')
+  state.loading = false
 }
 </script>
 
 <template>
   <div class="relative flex flex-col">
     <back-header @back="router.push('/')">消息发送</back-header>
-    <div v-for="d in data" :key="d[0]" class="ml-16 mt-4">
-      <label class="block">
-        <p class="my-2 text-md font-bold mt-4">{{ d[0] }}</p>
-        <input class="px-2 py-1 shadow appearance-none border rounded block" type="text" :placeholder="d[1]"
-          v-model="d[2]">
-      </label>
+    <div>
+      <div class="my-2 mx-4">
+        <label class="block">
+          <p class="my-2 text-md font-bold mt-4">用户ID</p>
+          <input class="px-2 py-1 shadow appearance-none border rounded block" type="text" placeholder="id1,id2,id3" v-model="uids">
+        </label>
+      </div>
+      <div v-for="d in data" :key="d[0]" class="my-2 mx-4">
+        <label class="block">
+          <p class="my-2 text-md font-bold mt-4">{{ d[0] }}</p>
+          <input class="px-2 py-1 shadow appearance-none border rounded block" type="text" :placeholder="d[1]" v-model="d[2]">
+        </label>
+      </div>
     </div>
-    <div class="px-2 py-1 border border-solid mt-10">
+    <!-- <div class="px-2 py-1 border border-solid mt-10">
       choose user
-    </div>
-    <div class="flex flex-wrap ml-16 mt-4">
-      <button class="card" @click="sendMsg">
-        <check-icon class="w-6 text-blue-500" />提交
-      </button>
-    </div>
+    </div> -->
+    <button class="all-transition bg-blue-500 font-bold text-white rounded-full shadow hover:shadow-md m-4 px-4 py-2 w-32" :class="ready ? 'bg-blue-500' : 'bg-gray-500'" @click="sendMsg" :disabled="!ready">确认提交</button>
   </div>
 </template>
-
-<style scoped>
-button.card {
-  @apply flex items-center rounded py-2 px-4 shadow-md bg-white hover:shadow-lg m-2 transition-all
-}
-</style>
