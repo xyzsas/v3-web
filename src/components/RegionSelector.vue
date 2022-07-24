5a369e97fa49871bc629f301e65da361c7e88d51
Phantomlsh <phantomlsh@outlook.com>
2022-07-24 08:11:46 -0700

    Add region selector

---
+++ src/components/RegionSelector.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { watch } from 'vue'
+const props = defineProps(['modelValue'])
+const emits = defineEmits(['update:modelValue'])
+
+let data = $ref(null), selected = $ref(['', '', ''])
+
+function parseValue () {
+  const v = props.modelValue
+  if (!data || !v) return
+  if (v === selected.join('')) return
+  for (const k in data) {
+    if (v.indexOf(k) === 0) {
+      selected[0] = k
+      break
+    }
+  }
+  if (!selected[0]) return
+  for (const k in data[selected[0]]) {
+    if (v.indexOf(k) === selected[0].length) {
+      selected[1] = k
+      break
+    }
+  }
+  if (!selected[1]) return
+  for (const k of data[selected[0]][selected[1]]) {
+    if (v.indexOf(k) === selected[0].length + selected[1].length) {
+      selected[2] = k
+      break
+    }
+  }
+}
+
+async function init () {
+  data = await fetch('https://cdn.jsdelivr.net/gh/modood/Administrative-divisions-of-China@latest/dist/pca.json').then(res => res.json()).catch(() => ({}))
+  parseValue()
+}
+init()
+
+watch(() => selected[2], v => {
+  emits('update:modelValue', v ? selected.join('') : '')
+})
+watch(() => props.modelValue, parseValue, { immediate: true })
+</script>
+
+<template>
+  <div class="flex items-center flex-wrap" v-if="data">
+    <select v-model="selected[0]" class="p-1 border" @change="selected[1] = selected[2] = ''">
+      <option value="">省</option>
+      <option v-for="(v, k) in data">{{ k }}</option>
+    </select>
+    <select v-model="selected[1]" v-if="selected[0]" class="p-1 border" @change="selected[2] = ''">
+      <option value="">市</option>
+      <option v-for="(v, k) in data[selected[0]]">{{ k }}</option>
+    </select>
+    <select v-model="selected[2]" v-if="selected[1]" class="p-1 border">
+      <option value="">区</option>
+      <option v-for="v in data[selected[0]][selected[1]]">{{ v }}</option>
+    </select>
+  </div>
+</template>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,9 +1,12 @@
 <script setup>
 import ScanInput from '../components/ScanInput.vue'
-let text = $ref('')
+let text = $ref(''), region = $ref('江苏省扬州市广陵区')
+import RegionSelector from '../components/RegionSelector.vue'
 </script>
 
 <template>
-  <p>{{ text }}</p>
   <scan-input v-model="text" class="w-96 px-3 py-1 m-4 bg-white" />
+  <hr class="my-4">
+  <p>{{ region }}</p>
+  <region-selector class="m-4" v-model="region" />
 </template>
