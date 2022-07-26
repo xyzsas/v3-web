8c2e5dced3839d1726f3a781df71881de4ff031a
Phantomlsh <phantomlsh@outlook.com>
2022-07-26 10:45:16 -0700

    Update AppProfile.vue

--- src/views/AppProfile.vue
+++ src/views/AppProfile.vue
@@ -22,57 +22,39 @@ let personalInfo1 = $ref({
 })
 
 
-let field = {
-  '个人信息': ['1', personalInfo], '家长信息': ['2', parentInfo], '个人信息2': ['3', personalInfo1], '家长信息2': ['4', parentInfo],
-  '个人信息3': ['5', personalInfo1], '家长信息4': ['6', parentInfo], '个人信息4': ['7', personalInfo1], '家长信息3': ['8', parentInfo]
-}
+const fields = [
+  { title: '个人信息', data: personalInfo, done: true },
+  { title: '个人信息2', data: personalInfo },
+  { title: '个人信息3', data: personalInfo },
+  { title: '个人信息4', data: personalInfo },
+  { title: '家长信息', data: parentInfo },
+  { title: '家长信息2', data: parentInfo },
+  { title: '家长信息3', data: parentInfo },
+  { title: '家长信息4', data: parentInfo }
+]
 
-let current = $ref(personalInfo)
-
-
+let current = $ref(0) // index of field
 </script>
 
 <template>
   <BackHeader @back="router.push('/')">个人资料</BackHeader>
-
-  <div class="overflow-x-auto bg-orange-50">
-    <div class="flex flex-nowrap">
-      <div v-for="(k, v) in field" class="my-4">
-        <div class="flex flex-col justify-center items-center ml-5">
-          <button class="text-white font-bond text-lg circle" :class="current === k[1] ? 'ring-4' : 'ring-0'"
-            @click="current = k[1]"> {{ k[0] }} </button>
-          <div class="text-xs whitespace-nowrap text-gray-500">{{ v }}</div>
+  <div class="overflow-x-auto bg-white border border-x-0 mb-4">
+    <div class="flex">
+      <div v-for="(v, i) in fields" class="my-4">
+        <div class="flex flex-col justify-center items-center mx-2 px-2 cursor-pointer" @click="current = i">
+          <button class="all-transition text-white font-bold text-lg rounded-full w-12 h-12 mb-1 text-center" :class="[current === i && 'ring', v.done ? 'bg-green-600' : 'bg-gray-500']">{{ i + 1 }}</button>
+          <div class="text-xs whitespace-nowrap text-gray-500">{{ v.title }}</div>
         </div>
       </div>
     </div>
   </div>
-  <div class="flex flex-col">
-    <div v-for="d in current" :key="d[0]" class="my-1 mx-4">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+    <div v-for="d in fields[current].data" class="my-2 mx-4">
       <label class="block">
-        <p class="text-md font-bold mt-4">{{ d[0] }}</p>
+        <p class="text-md font-bold">{{ d[0] }}</p>
         <input class="p-2 shadow appearance-none border rounded block w-full" type="text" :placeholder="d[1]"
           v-model="d[2]">
       </label>
     </div>
   </div>
 </template>
-
-<style>
-button.circle {
-  height: 45px;
-  width: 45px;
-  border-radius: 50%;
-  border: 1px;
-  background-color: grey;
-  margin-bottom: 5px;
-}
-
-button.finish {
-  height: 45px;
-  width: 45px;
-  border-radius: 50%;
-  border: 1px;
-  background-color: green;
-  margin-bottom: 5px;
-}
-</style>
