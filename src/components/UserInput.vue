3a2a930b700d0678b1eb79ee1e6fa619d1103461
Phantomlsh <phantomlsh@outlook.com>
2022-07-29 19:52:03 -0700

    Fix UserInput

--- src/components/UserInput.vue
+++ src/components/UserInput.vue
@@ -7,44 +7,47 @@ import srpc from '../utils/srpc-fc.js'
 import state from '../state.js'
 import ScanInput from './ScanInput.vue'
 
-let loading = $ref(false), text = $ref(''), userInfo = $ref({})
+let text = $ref(''), id = $ref(''), tip = $ref(' ')
 
 const props = defineProps(['modelValue'])
 const emits = defineEmits(['update:modelValue'])
 
-async function fetchInfo () {
-  userInfo = {}
-  if (!text) return
-  loading = true
-  let id = text
-  if (Number.isInteger(Number(text))) {
-    id = await sha256(text + 'CHAOXING')
-    id = id.substring(0, 16).replaceAll('/', '_').replaceAll('+', '-')
-  }
-  const res = await srpc.X.queryUser(state.user.token, id)
-  if (res && res[id]) userInfo = res[id]
-  else userInfo.error = '用户不存在'
-  loading = false
-}
 const parseDate = t => moment(t).fromNow()
-const fetchUser = debounce(fetchInfo, 600)
 
-watch($$(text), v => {
+async function fetchInfo () {
+  if (!id) return
+  tip = '' // loading
+  const res = await srpc.X.queryUser(state.user.token, id)
+  if (res && res[id]) tip = `${res[id].name} (${res[id].uid}) ${parseDate(res[id].time)}`
+  else tip = '用户不存在'
+}
+const fetchUser = debounce(fetchInfo, 1000)
+
+// emits and fetch
+watch($$(id), async v => {
+  tip = ' '
   fetchUser()
-  emits('update:modelValue', v)
+  emits('update:modelValue', id)
 })
+// update text by id
 watch(() => props.modelValue, v => {
-  if (text !== v) text = v
+  if (id !== v) text = v
+})
+// update id by text
+watch($$(text), async v => {
+  if (!v) return id = ''
+  if (Number.isInteger(Number(v))) {
+    const res = await sha256(v + 'CHAOXING')
+    return id = res.substring(0, 16).replaceAll('/', '_').replaceAll('+', '-')
+  }
+  id = v
 })
 </script>
 
 <template>
   <div class="relative">
     <ScanInput v-model="text" class="w-full px-2 py-1 bg-white" placeholder="用户id/超星号" />
-    <img v-if="loading" :src="spinner" class="w-5 m-1">
-    <div v-if="userInfo?.name" class="px-2 py-1 text-xs">
-      {{ userInfo.name }} ({{ userInfo.uid }}) {{ parseDate(userInfo.time) }}
-    </div>
-    <p v-if="userInfo?.error" class="px-2 py-1 text-xs">{{ userInfo.error }}</p>
+    <p v-if="tip" class="px-2 py-1 text-xs">{{ tip }}</p>
+    <img v-else :src="spinner" class="w-5 m-1">
   </div>
 </template>
