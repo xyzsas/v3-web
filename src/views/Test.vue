75eea0f49b48f946f20cca897fbb2721a59fdc8e
Phantomlsh <phantomlsh@outlook.com>
2022-07-29 20:31:02 -0700

    Add EditableList

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -3,9 +3,16 @@ import UserInput from '../components/UserInput.vue'
 import RegionSelector from '../components/RegionSelector.vue'
 import spinner from '../assets/spinner.svg'
 import Toggle from '../components/Toggle.vue'
+import EditableList from '../components/EditableList.vue'
 
 let toggle = $ref(false)
 let region = $ref('江苏省扬州市广陵区')
+
+let list = $ref([
+  { k: 'a', value: 'A' },
+  { k: 'b', value: 'B' },
+  { k: 'c', value: 'C' }
+])
 </script>
 
 <template>
@@ -13,5 +20,14 @@ let region = $ref('江苏省扬州市广陵区')
   <hr class="my-4">
   <p>{{ region }}</p>
   <RegionSelector class="m-4" v-model="region" />
-  <toggle v-model="toggle">Test</toggle>
+  <Toggle v-model="toggle">Test</Toggle>
+  <hr class="my-4">
+  <EditableList :list="list" item-key="k" class="w-64 m-4" item-class="bg-white my-1 p-1 shadow">
+    <template #item="{ element: el }">
+      <div class="mx-2">
+        <label>{{ el.value }}</label>
+        <input type="number" v-model="el.cot" class="border ml-2 w-20">
+      </div>
+    </template>
+  </EditableList>
 </template>
