fe6048c15af80105c4d235473aa6567037c05c8e
VampireRabbit <1662025808@qq.com>
2022-08-10 17:40:41 +0800

    Update ProgressBar.vue

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,14 +1,14 @@
 <script setup>
+import { computed } from '@vue/reactivity';
 const { ratio } = defineProps(['ratio'])
-const bar = `${(ratio*100).toFixed(1)}%`
+const bar = computed(() => `${(ratio*100).toFixed(1)}%`) 
 </script>
 
 <template>
   <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-    <div v-if="ratio > 0.5" class="bg-blue-500 text-right font-bold text-blue-100 text-center p-1 leading-none rounded-full" :style="`width: ${bar}`">{{ bar }}</div>
-    <div v-else class="flex">
-      <div class="bg-blue-500 text-right font-bold text-blue-100 text-center p-1 leading-none rounded-full" :style="`width: ${bar}`"></div>
-      <div class="font-bold text-black px-1">{{ bar }}</div>
+    <div class="flex text-black">
+      <div class="bg-blue-500 text-right font-bold p-1 leading-none rounded-full" :style="`width: ${bar}`"></div>
+      <div class="font-bold px-1 relative" :class="ratio > 0.5 && 'right-16 text-white'">{{ bar }}</div>
     </div>
   </div>
 </template>
