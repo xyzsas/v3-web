c83962ce96a8d07f88bee68c00d31f16caa235a7
Layzzi <Shadow_sock@icloud.com>
2022-08-14 18:04:09 +0800

    side window v-model & animation

---
+++ src/components/UserSelector.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { watch } from 'vue'
+const props = defineProps(['modelValue'])
+const emits = defineEmits(['update:modelValue'])
+let model = $ref(props.modelValue)
+
+watch($$(model), v => {
+  emits('update:modelValue', v)
+})
+
+watch(() => props.modelValue, v => {
+  if (model !== v) model = v
+})
+
+</script>
+
+<template>
+  <div class="fixed top-0 w-96 bg-white min-h-screen all-transition" :class="model.show ? 'right-0' : '-right-96'">
+    <button @click="model.show = false">click</button>
+  </div>
+</template>
