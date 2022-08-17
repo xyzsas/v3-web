bdde7d344884bf95675c6853599cb575eff13481
Layzzi <Shadow_sock@icloud.com>
2022-08-17 12:59:26 +0800

    Update UserSelector.vue

--- src/components/UserSelector.vue
+++ src/components/UserSelector.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { watch } from 'vue'
+import { XIcon } from '@heroicons/vue/solid'
 const props = defineProps(['modelValue'])
 const emits = defineEmits(['update:modelValue'])
 let model = $ref(props.modelValue)
@@ -16,6 +17,6 @@ watch(() => props.modelValue, v => {
 
 <template>
   <div class="fixed top-0 w-96 bg-white min-h-screen all-transition" :class="model.show ? 'right-0' : '-right-96'">
-    <button @click="model.show = false">click</button>
+    <XIcon class="absolute top-0 right-0 cursor-pointer w-8 m-2 text-gray-600" @click="model.show = false" />
   </div>
 </template>
