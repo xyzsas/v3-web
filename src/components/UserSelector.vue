14dd27cf9b0100fb96c398e7c4bdac01aecb66d6
Layzzi <Shadow_sock@icloud.com>
2022-08-25 21:18:17 +0800

    Update UserSelector.vue

--- src/components/UserSelector.vue
+++ src/components/UserSelector.vue
@@ -3,13 +3,14 @@ import { watch } from 'vue'
 import { XIcon, CheckIcon, PlusIcon, ChevronLeftIcon, TrashIcon } from '@heroicons/vue/solid'
 import { _id, query, search } from '../utils/user.js'
 import Wrapper from '../components/Wrapper.vue'
+import state from '../state.js'
 const props = defineProps(['modelValue'])
 const emits = defineEmits(['update:modelValue'])
 let model = $ref(props.modelValue)
 let keyword = $ref(''), mode = $ref(0)
 let selected = $ref({}), result = $ref({})
 
-let showResult = $ref(false), showSelected = $ref(false), allSelected = $ref(false)
+let showResult = $ref(false), allSelected = $ref(false)
 
 const placeholder = ['输入用户id', '输入以逗号分隔的用户id列表', '输入JSON对象或使用下方选择器']
 const filter = $ref({})
@@ -22,7 +23,6 @@ init()
 async function refresh () {
   model.show = false
   showResult = false
-  showSelected = false
   mode = 0
   result = {}
   selected = await query(model.list)
@@ -43,6 +43,7 @@ const parseBg = x => {
 }
 
 async function modeSearch () {
+  showResult = false
   if (mode == 0) {
     try {
       result = await _id(keyword)
@@ -128,56 +129,53 @@ function submit () {
       <div class="all-transition px-2 rounded border border-blue-400 font-bold cursor-pointer m-1" :class="mode === 2 ? 'bg-blue-400 text-white' : 'bg-blue-100 text-blue-400'" @click="mode = 2">高级搜索</div>
     </div>
     <input class="border my-2 w-full rounded py-1 px-2" v-model="keyword" :placeholder="placeholder[mode]">
-    <div v-if="mode === 2" class="flex flex-wrap items-center my-2 font-mono">
+    <div v-if="mode === 2" class="flex flex-col my-2 font-mono">
       <div class="flex items-center m-1">
         年级
-        <input class="border rounded py-1 px-2 w-16 ml-1" v-model="filter['年级']">
+        <input class="border rounded py-1 px-2 mx-1" v-model="filter['年级']">
       </div>
       <div class="flex items-center m-1">
         班级
-        <input class="border rounded py-1 px-2 w-16 ml-1" v-model="filter['班级']">
+        <input class="border rounded py-1 px-2 mx-1" v-model="filter['班级']">
       </div>
       <div class="flex items-center m-1">
         姓名
-        <input class="border rounded py-1 px-2 w-20 ml-1" v-model="filter['姓名']">
+        <input class="border rounded py-1 px-2 mx-1" v-model="filter['姓名']">
       </div>
       <div class="flex items-center m-1">
         学号
-        <input class="border rounded py-1 px-2 w-16 ml-1" v-model="filter['学号']">
+        <input class="border rounded py-1 px-2 mx-1" v-model="filter['学号']">
       </div>
     </div>
     <button class="all-transition font-bold text-white rounded-full shadow hover:shadow-md my-2 px-2 py-1 w-16" :class="parseBg(mode)" @click="modeSearch">搜索</button>
     <div v-if="true" class="rounded bg-gray-100 py-1 px-2 my-2">
       <div class="m-1 flex items-center">
         搜索结果
-        <div v-if="Object.keys(result).length" class="text-sm flex items-center all-transition font-mono shadow rounded p-1 mx-1 cursor-pointer" @click="selectAll" :class="allSelected ? 'bg-blue-500 text-white' : 'bg-white text-gray-600'">
-          <PlusIcon v-if="!allSelected" class="w-4 text-blue-500" />
-          <CheckIcon v-else class="w-4 text-white" />
-          全选
+        <div v-if="Object.keys(result).length" class="text-sm flex items-center font-mono cursor-pointer" @click="selectAll">
+          <PlusIcon v-if="!allSelected" class="w-4 text-gray-400 rounded bg-white border-2 m-1" />
+          <CheckIcon v-else class="w-4 text-white rounded bg-blue-500 m-1" />
+          全选({{ Object.keys(result).length }}项)
         </div>
         <div class="grow"></div>
         <ChevronLeftIcon class="all-transition w-8 cursor-pointer" :class="showResult ? '-rotate-90' : 'rotate-0'" @click="showResult = !showResult"/>
       </div>
       <Wrapper :show="showResult">
-        <div class="px-2 py-1">
-          <div v-for="(v, k) in result" class="flex items-center all-transition font-mono shadow rounded px-1 my-1 cursor-pointer" @click="select(k)" :class="selected[k] ? 'bg-blue-500 text-white' : 'bg-white text-gray-600'">
-            <PlusSmIcon v-if="!selected[k]" class="w-6 text-blue-500" />
-            <CheckIcon v-else class="w-6 text-white" />
-            <div class="m-1">{{ v['姓名'] }}</div>
-            <div class="m-1">{{ v['年级'] }}</div>
-            <div class="m-1">{{ v['班级'] }}</div>
-            <div class="m-1">{{ v['学号'] }}</div>
+        <div class="px-2 py-1 max-h-96 overflow-y-scroll">
+          <div v-for="(v, k) in result" class="flex items-center text-gray-600 font-mono px-1 border-b border-l cursor-pointer" @click="select(k)">
+            <PlusIcon v-if="!selected[k]" class="w-4 text-gray-400 rounded bg-white border-2" />
+            <CheckIcon v-else class="w-4 text-white rounded bg-blue-500" />
+            <div class="mx-1">{{ v['姓名'] }}</div>
+            <div class="mx-1">{{ v['年级'] }}</div>
+            <div class="mx-1">{{ v['班级'] }}</div>
+            <div class="mx-1">{{ v['学号'] }}</div>
           </div>
         </div>
       </Wrapper>
     </div>
-    <div class="rounded bg-gray-100 py-3 px-3 my-2 flex items-center">
-      已选择 {{ Object.keys(selected).length }} 名用户
-      <div class="text-sm flex items-center all-transition font-mono shadow rounded p-1 mx-1" @click="selected = {}" :class="Object.keys(selected).length ? 'bg-red-500 text-white cursor-pointer' : 'bg-white text-gray-600 cursor-default'">
-          <TrashIcon class="w-4" :class="Object.keys(selected).length ? 'text-white' : 'text-red-500'"/>
-          清空
-        </div>
+    <div class="flex items-center">
+      <button class="all-transition font-bold text-white bg-blue-500 rounded-full shadow hover:shadow-md m-2 px-2 py-1 w-16" @click="submit">提交</button>
+      <button class="all-transition font-bold text-white bg-red-500 rounded-full shadow hover:shadow-md m-2 px-2 py-1 w-16" @click="selected = {}">清空</button>
+      <div class="text-sm font-mono">已选中 {{ Object.keys(selected).length }} 名用户</div>
     </div>
-    <button class="all-transition font-bold text-white bg-blue-500 rounded-full shadow hover:shadow-md my-2 px-2 py-1 w-16" @click="submit">提交</button>
   </div>
 </template>
