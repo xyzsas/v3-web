c23c1206d40b0750a9ecc60421f6ad2eb3ff277d
Phantomlsh <phantomlsh@outlook.com>
2022-10-03 18:55:13 -0700

    Upgrade EditableList

--- src/components/EditableList.vue
+++ src/components/EditableList.vue
@@ -1,19 +1,29 @@
 <script setup>
-import Draggable from 'vuedraggable'
+import { onMounted } from 'vue'
+import Sortable from 'sortablejs/modular/sortable.core.esm.js'
 import { TrashIcon, Bars2Icon } from '@heroicons/vue/24/outline'
-const props = defineProps(['list', 'itemKey', 'itemClass', 'disableDrag'])
+const props = defineProps(['list', 'itemClass', 'disableDrag'])
+let el = $ref()
+
+onMounted(() => {
+  const sortable = new Sortable(el, {
+    handle: '.handle',
+    animation: 150,
+    onUpdate: e => {
+      props.list.splice(e.newIndex, 0, props.list.splice(e.oldIndex, 1)[0])
+    }
+  })
+})
 </script>
 
 <template>
-  <Draggable :list="props.list" handle=".handle" :item-key="props.itemKey">
-    <template #item="{ element, index: i }">
-      <div class="flex flex-wrap justify-between items-center" :class="props.itemClass">
-        <slot name="item" :element="element" :index="i"></slot>
-        <div class="flex">
-          <TrashIcon class="w-4 mx-2 cursor-pointer text-red-500" @click="props.list.splice(i, 1)"/>
-          <Bars2Icon v-if="!props.disableDrag" class="w-4 cursor-pointer handle text-gray-500" />
-        </div>
-      </div> 
-    </template>
-  </Draggable>
+  <div ref="el">
+    <div v-for="(e, i) in props.list" class="flex flex-wrap justify-between items-center" :class="props.itemClass" :key="e">
+      <slot name="item" :element="e" :index="i"></slot>
+      <div class="flex">
+        <TrashIcon class="w-4 mx-2 cursor-pointer text-red-500" @click="props.list.splice(i, 1)"/>
+        <Bars2Icon v-if="!props.disableDrag" class="w-4 cursor-pointer handle text-gray-500" />
+      </div>
+    </div>
+  </div>
 </template>
