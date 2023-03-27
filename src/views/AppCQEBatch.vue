6e16c9b1c77a07b7dd7be9b1ecbc1dc8601c97d1
Layzzi <33226552+SiuGaaLei@users.noreply.github.com>
2023-03-27 23:50:44 +0800

    items design

--- src/views/AppCQEBatch.vue
+++ src/views/AppCQEBatch.vue
@@ -7,18 +7,15 @@ import local from '../utils/srpc-local.js'
 import fc from '../utils/srpc-fc.js'
 import { search } from '../utils/user.js'
 import { CheckIcon, PlusIcon } from '@heroicons/vue/24/solid'
+import { T } from '../utils/CQE.js'
 const router = useRouter()
+
+const terms = ['高一上学期', '高一下学期', '高二上学期', '高二下学期', '高三上学期', '高三下学期']
+const indexName = ['分项积分', '班级评价', '年级评价', '学校评价']
 
 let showUser = $ref(false), showItem = $ref(false), showCredit = $ref(false)
 let user = $ref({}), admin = $ref({})
-let filter = $ref({
-  '高一第一学期': {},
-  '高一第二学期': {},
-  '高二第一学期': {},
-  '高二第二学期': {},
-  '高三第一学期': {},
-  '高三第二学期': {}
-})
+let filter = $ref({}), index = $ref(false)
 
 function selectAll () {
   let sum = 0
@@ -60,26 +57,60 @@ async function init () {
     const us = await search(filter)
     for (const u in us) user[u] = 0
     admin = {
-      index: data.index,
+      index: data.index.split(','),
       class: data.class.split(',').map(x => data.grade + '/' + x),
       users: us,
       ids: Object.keys(us).sort((a, b) => us[a].年级 + us[a].班级 + us[a].学号 < us[b].年级 + us[b].班级 + us[b].学号 ? -1 : 1)
     }
   }
-  console.log(admin)
+  for (const k in T) {
+    for (const i in T[k]) filter[i] = false
+  }
 }
 init()
 
 </script>
 
 <template>
-  <BackHeader @back="router.push('/')">综合素质评价-批量打分</BackHeader>
+  <div class="flex items-center">
+    <BackHeader @back="router.push('/')">综合素质评价-批量打分</BackHeader>
+    <select v-model="term" class="py-1 px-2 rounded border text-sm font-bold m-1">
+      <template v-for="(t, i) in terms">
+        <option :value="i">{{ t }}</option>
+      </template>
+    </select>
+    <select v-model="index" class="py-1 px-2 rounded border text-sm font-bold m-1">
+      <template v-for="i in admin.index">
+        <option :value="i">{{ indexName[i] }}</option>
+      </template>
+    </select>
+  </div>
   <div class="md:flex">
     <div class="md:w-2/3">
       <div class="rounded bg-white shadow-md px-4 py-2 m-2 sm:mx-8 sm:my-4">
         <div class="text-2xl font-bold m-1" @click="showItem = !showItem">项目</div>
         <Wrapper :show="showItem" style="max-height: 80vh; overflow-y: auto;">
-          view design
+          <div>
+            <div v-for="(items, k) in T">
+              <div class="px-2 border-l-4 border-blue-500 text-xl my-2">{{ k }}</div>
+              <div class="flex items-center flex-wrap whitespace-nowrap my-2">
+                <div v-for="(v, i) in T[k]" class="py-2 px-2 rounded font-bold border-2 m-2 flex items-center all-transition" :class="filter[i] ? 'border-blue-400 bg-blue-100' : 'border-gray-200 cursor-pointer'" @click="filter[i] = filter[i] || { value: 0, copy: -1 }">
+                  <div class="mx-2" :class="filter[i] ? 'text-blue-400' : 'text-gray-700'">{{ i }}</div>
+                  <div v-if="filter[i]" class="flex items-center">
+                    <div class="text-gray-700">{{ indexName[index] }}: </div>
+                    <input v-if="filter[i].copy === -1" type="number" v-model="filter[i].value" class="py-1 px-2 rounded border text-sm font-bold mx-1 w-24">
+                    <select v-else v-model="filter[i].copy" class="py-1 px-2 rounded border text-sm font-bold mx-1 w-24">
+                      <template v-for="(n, i) in indexName">
+                        <option :value="i">{{ n }}</option>
+                      </template>
+                    </select>
+                    <button class="rounded mx-1 px-2 py-1 text-sm font-bold all-transition" :class="filter[i].copy === -1 ? 'bg-yellow-100 text-yellow-400' : 'bg-yellow-400 text-white'" @click.stop="filter[i].copy = filter[i].copy === -1 ? 0 : -1">继承</button>
+                    <button class="rounded mx-1 px-2 py-1 text-sm font-bold bg-red-100 text-red-400" @click.stop="filter[i] = false">取消</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </Wrapper>
       </div>
       <div class="rounded bg-white shadow-md px-4 py-2 m-2 sm:mx-8 sm:my-4">
