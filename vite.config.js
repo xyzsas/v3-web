816926bd2607bcadb39ecfd0911088c9619ce214
Phantomlsh <phantomlsh@outlook.com>
2023-05-18 22:01:38 +0000

    upgrade vue-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import reactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue({ reactivityTransform: true })]
+  plugins: [vue(), reactivityTransform()]
 })
