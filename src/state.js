b59fb1d1c4bb4edbcb7cbd6ec4f936b124e3b9f9
Phantomlsh <phantomlsh@outlook.com>
2023-12-08 20:58:33 +0000

    Update login session storage

--- src/state.js
+++ src/state.js
@@ -1,11 +1,28 @@
-import { reactive, watch } from 'vue'
+import { reactive } from 'vue'
 
 export const SS = window.sessionStorage
 
 export const state = reactive({
   loading: false,
-  user: SS.user ? JSON.parse(SS.user) : null,
+  user: null,
   msg: null
 })
+
+export function login (token) {
+  SS.removeItem('token')
+  let input = token?.split('.')?.[1]
+  if (!input) return false
+  input = input.replace(/-/g, '+').replace(/_/g, '/')
+  const pad = input.length % 4
+  if (pad) input += new Array(5 - pad).join('=')
+  const payload = JSON.parse(decodeURIComponent(atob(input).split('').map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join('')))
+  if (payload.iat + 86400e3 < Date.now()) return false
+  payload.token = token
+  SS.token = token
+  state.user = payload
+  return payload
+}
+
+if (SS.token) login(SS.token)
 
 export default state
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,10 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
-import { state, SS } from '../state'
+import { login } from '../state'
 const route = useRoute(), router = useRouter()
 const token = route.query.token
 
-function decode (input) {
-  if (!input) return {}
-  input = input.replace(/-/g, '+').replace(/_/g, '/')
-  const pad = input.length % 4
-  if (pad) input += new Array(5 - pad).join('=')
-  return JSON.parse(decodeURIComponent(atob(input).split('').map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join('')))
-}
-
-if (token) {
-  state.user = decode(token.split('.')[1])
-  state.user.token = token
-  SS.user = JSON.stringify(state.user)
-}
+login(token)
 router.push('/')
 </script>
 
